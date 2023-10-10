01b30be9f38d80c78acd0cda235f2701360272b3
Muba from ubuntu <mubaraksakariya@gmail.com>
2023-10-10 13:05:00 +0530

    some edits

--- FrontEnd/src/Componants/User/ContactForm.jsx
+++ FrontEnd/src/Componants/User/ContactForm.jsx
@@ -1,35 +1,63 @@
-import React from 'react'
-import './ContactForm.css'
+import React from 'react';
+import './ContactForm.css';
 
 function ContactForm() {
-    return (
-        <div className='contact-top-div'>
-            <div className="row contact-div">
-                <div className="col-md-6 contact-img-div">
-                    <img src="./assets/office.jpg" alt="" className='contact-img' />
-                </div>
-                <div className="col-md-6 d-flex justify-content-center align-items-center ">
-                    <form action="" className='contact-form'>
-                        <div className="mb-3 ">
-                            <h3>Quick Enquiry</h3>
-                        </div>
-                        <div className="mb-3">
-                            <input type="text" className="form-control contact-input-feild" name="" id="" aria-describedby="helpId" placeholder="Name" />
-                        </div>
-                        <div className="mb-3">
-                            <input type="text" className="form-control contact-input-feild" name="" id="" aria-describedby="emailHelpId" placeholder="Email/Phone" />
-                        </div>
-                        <div className="mb-3">
-                            <textarea className="form-control contact-input-feild" name="" id="" rows="3" placeholder='Message' />
-                        </div>
-                        <div className="">
-                            <button type='submit' className='contact-submit-button btn btn-lg btn-outline-success px-4'>send</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className='contactForm-top-div'>
+			<div className='row contact-div'>
+				<div className='col-md-6 contact-img-div'>
+					<img
+						src='./assets/office.jpg'
+						alt=''
+						className='contact-img'
+					/>
+				</div>
+				<div className='col-md-6 d-flex justify-content-center align-items-center '>
+					<form action='' className='contact-form'>
+						<div className='mb-3 '>
+							<h3>Quick Enquiry</h3>
+						</div>
+						<div className='mb-3'>
+							<input
+								type='text'
+								className='form-control contact-input-feild'
+								name=''
+								id=''
+								aria-describedby='helpId'
+								placeholder='Name'
+							/>
+						</div>
+						<div className='mb-3'>
+							<input
+								type='text'
+								className='form-control contact-input-feild'
+								name=''
+								id=''
+								aria-describedby='emailHelpId'
+								placeholder='Email/Phone'
+							/>
+						</div>
+						<div className='mb-3'>
+							<textarea
+								className='form-control contact-input-feild'
+								name=''
+								id=''
+								rows='3'
+								placeholder='Message'
+							/>
+						</div>
+						<div className=''>
+							<button
+								type='submit'
+								className='contact-submit-button btn btn-lg btn-outline-success px-4'>
+								send
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default ContactForm
+export default ContactForm;
--- FrontEnd/src/Componants/User/Footer.jsx
+++ FrontEnd/src/Componants/User/Footer.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 function Footer() {
-    return (
-        <div className="continer footer-main-div text-white">
-            <div className='address'>
-                1st Floor, MPH Tower, Opposite Bharat petroleum, Edavanna 676541 <br /> +91 9995745200 | info@hayathtravels.com
-            </div>
-            <div className='copyright'>
-                Copyright © 2023 hayath travels
-            </div>
-        </div>
-    )
+	return (
+		<div className='continer footer-main-div text-white'>
+			<div className='address'>
+				1st Floor, MPH Tower, Opposite Bharat petroleum, Edavanna 676541{' '}
+				<br /> +91 9995745200 | info@hayathtravels.com
+			</div>
+			<div className='footer-socials'>
+				<div className='icon-round-div'>
+					<i className='bi bi-facebook'></i>
+				</div>
+				<div className='icon-round-div'>
+					<i className='bi bi-instagram'></i>
+				</div>
+				<div className='icon-round-div'>
+					<i className='bi bi-twitter'></i>
+				</div>
+			</div>
+			<div className='copyright'>Copyright © 2023 hayath travels</div>
+		</div>
+	);
 }
 
-export default Footer
+export default Footer;
