caead126a7a4a0aae5808507d3dce32ce2f6ea11
Muba from ubuntu <mubaraksakariya@gmail.com>
2023-10-17 22:45:12 +0530

    style changes

--- FrontEnd/src/Componants/User/FolderThumpnail.jsx
+++ FrontEnd/src/Componants/User/FolderThumpnail.jsx
@@ -3,25 +3,28 @@ import PropTypes from 'prop-types';
 import './FolderThumpnail.css';
 
 const FolderThumpnail = ({ folder, setFolderView }) => {
-    const divStyle = {
-        backgroundImage: `url(${folder.thumpnail})`,
-    };
+	const divStyle = {
+		backgroundImage: `url(${folder.thumpnail})`,
+	};
 
-    const handleFolderClick = () => {
-        setFolderView(folder);
-    };
+	const handleFolderClick = () => {
+		setFolderView(folder);
+	};
 
-    return (
-        <div className="folder-thumb-div" style={divStyle} onClick={handleFolderClick}>
-            <div className="folder-overlay"></div>
-            <h4 className='folder-thumb-h4'>Inauguration</h4>
-        </div>
-    );
+	return (
+		<div
+			className='folder-thumb-div'
+			style={divStyle}
+			onClick={handleFolderClick}>
+			<div className='folder-overlay'></div>
+			<h4 className='folder-thumb-h4'>Inauguration</h4>
+		</div>
+	);
 };
 
 FolderThumpnail.propTypes = {
-    folder: PropTypes.object.isRequired,
-    setFolderView: PropTypes.func.isRequired,
+	folder: PropTypes.object.isRequired,
+	setFolderView: PropTypes.func.isRequired,
 };
 
 export default FolderThumpnail;
