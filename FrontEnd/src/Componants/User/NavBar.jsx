faaaecee50fdadb86019337090fcf60309416b6b
Muba from ubuntu <mubaraksakariya@gmail.com>
2023-10-19 11:13:25 +0530

    gallery link change ,removed #

--- FrontEnd/src/Componants/User/NavBar.jsx
+++ FrontEnd/src/Componants/User/NavBar.jsx
@@ -1,55 +1,101 @@
-import React, { useState } from 'react'
-import './NavBar.css'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import './NavBar.css';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function NavBar() {
-    const navigate = useNavigate()
-    const location = useLocation();
-    const { pathname } = location;
-    return (
-        <nav className="navbar navbar-expand-lg navbar-top-div p-0">
-            <div className="container">
-                <a className="navbar-brand" href="#"
-                    onClick={(e) => {
-                        e.preventDefault()
-                        navigate('/')
-                    }}
-                ><img src="./assets/Logo-Nobg.png" alt="" className='Logo-img' /></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end w-100">
-                        <li className="nav-item"
-                            onClick={() => navigate('/')}
-                        >
-                            <a className={`nav-link ${pathname === '/' ? 'current-page' : ''}`} aria-current="page">Home</a>
-                        </li>
-                        <li className="nav-item"
-                            onClick={() => navigate('/about')}
-                        >
-                            <a className={`nav-link ${pathname === '/about' ? 'current-page' : ''}`}>About</a>
-                        </li>
-                        <li className="nav-item"
-                            onClick={() => navigate('/services')}
-                        >
-                            <a className={`nav-link ${pathname === '/services' ? 'current-page' : ''}`}>Services</a>
-                        </li>
-                        <li className="nav-item"
-                            onClick={() => navigate('/contact')}
-                        >
-                            <a className={`nav-link ${pathname === '/contact' ? 'current-page' : ''}`}>Contact</a>
-                        </li>
-                        <li className="nav-item"
-                            onClick={() => navigate('/gallery')}
-                        >
-                            <a className={`nav-link ${pathname === '/gallery' ? 'current-page' : ''}`} href="#">Gallery</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+	const navigate = useNavigate();
+	const location = useLocation();
+	const { pathname } = location;
+	return (
+		<nav className='navbar navbar-expand-lg navbar-top-div p-0'>
+			<div className='container'>
+				<a
+					className='navbar-brand'
+					href='#'
+					onClick={(e) => {
+						e.preventDefault();
+						navigate('/');
+					}}>
+					<img
+						src='./assets/Logo-Nobg.png'
+						alt=''
+						className='Logo-img'
+					/>
+				</a>
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-bs-toggle='collapse'
+					data-bs-target='#navbarSupportedContent'
+					aria-controls='navbarSupportedContent'
+					aria-expanded='false'
+					aria-label='Toggle navigation'>
+					<span className='navbar-toggler-icon'></span>
+				</button>
+				<div
+					className='collapse navbar-collapse'
+					id='navbarSupportedContent'>
+					<ul className='navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end w-100'>
+						<li className='nav-item' onClick={() => navigate('/')}>
+							<a
+								className={`nav-link ${
+									pathname === '/' ? 'current-page' : ''
+								}`}
+								aria-current='page'>
+								Home
+							</a>
+						</li>
+						<li
+							className='nav-item'
+							onClick={() => navigate('/about')}>
+							<a
+								className={`nav-link ${
+									pathname === '/about' ? 'current-page' : ''
+								}`}>
+								About
+							</a>
+						</li>
+						<li
+							className='nav-item'
+							onClick={() => navigate('/services')}>
+							<a
+								className={`nav-link ${
+									pathname === '/services'
+										? 'current-page'
+										: ''
+								}`}>
+								Services
+							</a>
+						</li>
+						<li
+							className='nav-item'
+							onClick={() => navigate('/contact')}>
+							<a
+								className={`nav-link ${
+									pathname === '/contact'
+										? 'current-page'
+										: ''
+								}`}>
+								Contact
+							</a>
+						</li>
+						<li
+							className='nav-item'
+							onClick={() => navigate('/gallery')}>
+							<a
+								className={`nav-link ${
+									pathname === '/gallery'
+										? 'current-page'
+										: ''
+								}`}>
+								Gallery
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
-export default NavBar
+export default NavBar;
